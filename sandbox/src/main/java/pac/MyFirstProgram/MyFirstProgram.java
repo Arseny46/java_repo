8fd5328a953b0fc5c06aa15e6ac68e9b5fa3ce76
Arseny <110763405+Arseny46@users.noreply.github.com>
2023-07-16 21:30:28 +0300

    Новые строки и знакомство с sout

---
+++ sandbox/src/main/java/pac/MyFirstProgram/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package pac.MyFirstProgram;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello, world!");
+    System.out.println(1.0 / 3.0);
+    System.out.println("Hello" + 5);
+  }
+
+}
