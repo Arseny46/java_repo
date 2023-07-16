c6f165929edff2db7276d5d0311f5f3b3a690521
Arseny <110763405+Arseny46@users.noreply.github.com>
2023-07-16 22:55:46 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/pac/MyFirstProgram/MyFirstProgram.java
+++ sandbox/src/main/java/pac/MyFirstProgram/MyFirstProgram.java
@@ -3,10 +3,25 @@ package pac.MyFirstProgram;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-
-    System.out.println("Hello, world!");
-    System.out.println(1.0 / 3.0);
-    System.out.println("Hello" + 5);
+    hello("world");
+    hello("user");
+    double len = 5;
+    System.out.println("Плащадь квадрата со стороной " + len + " = " + area(len));
+    double a = 4;
+    double b = 6;
+    System.out.println("Плащадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
   }
 
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
